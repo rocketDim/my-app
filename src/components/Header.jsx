7b4dce23f010023eec284e188778e853ea59e185
rocketDim <dmitriydem81@mail.ru>
2023-07-17 00:57:42 +0300

    Разбил на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo.png' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My post
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+            <div>Post_1</div>
+            <div>Post_2</div>
+        </div>
+    </div>
+}
+
+export default Profile;
