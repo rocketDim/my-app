7b29f48aed61852264a1bcd723dea03a2e535d5b
rocketDim <dmitriydem81@mail.ru>
2023-07-20 00:27:50 +0300

    рефакторинг

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">Dmitriy</NavLink>
+                </div>
+
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Andrey</NavLink>
+                </div>
+
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Svetlana</NavLink>
+                </div>
+
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Cheburashka</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
+
     )
 }
 
