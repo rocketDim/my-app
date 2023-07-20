304d72294654285d34d17cdba9a16d3199199c68
rocketDim <dmitriydem81@mail.ru>
2023-07-21 00:00:51 +0300

    Рефакторинг кода

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
+
+const Profile = () => {
+    return (<div>
+        <ProfileInfo/>
+        <div>
+            <img
+                src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <MyPosts/>
+    </div>)
+}
+
+export default Profile;
